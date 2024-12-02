70a5a1b05c6f30b716b8c0520ecde3557d9607e7
sushmithanat <sushmithanat@gmail.com>
2024-12-02 12:51:55 +0530

    PostConstruct Annot from Jakarta

---
+++ PostConstructAnnotDemo/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,26 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(String name){
+        System.out.println("Hello from "+name);
+    }
+
+    @PostConstruct
+    public void initialize(){
+        this.name = "Honda";
+    }
+}
---
+++ PostConstructAnnotDemo/src/main/java/com/example/main/Example.java
@@ -0,0 +1,14 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name : "+vehicle.getName());
+        vehicle.printHello("UK");
+    }
+}
