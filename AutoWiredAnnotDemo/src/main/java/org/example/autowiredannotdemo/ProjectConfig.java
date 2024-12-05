d864e29af18c1b889a7ae7df2741e4f04f54a1e7
sushmithanat <sushmithanat@gmail.com>
2024-12-05 10:53:16 +0530

    Autowiring on field level

---
+++ AutoWiredAnnotDemo/src/main/java/org/example/autowiredannotdemo/ProjectConfig.java
@@ -0,0 +1,11 @@
+package org.example.autowiringbeanapproach;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan()
+public class ProjectConfig {
+
+}
---
+++ AutoWiredAnnotDemo/src/main/java/org/example/autowiredannotdemo/Vehicle.java
@@ -0,0 +1,27 @@
+package org.example.autowiringbeanapproach;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    public Vehicle(){
+        System.out.println("Vehicle bean created by Spring");
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
