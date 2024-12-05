705139c173fc9974a8f27fa2d623fb5a7cc1388e
sushmithanat <sushmithanat@gmail.com>
2024-12-05 10:39:39 +0530

    Autowiring using method Parameter

---
+++ AutoWiringBeanMethoParameterApproach/src/main/java/org/example/autowiringbeanapproach/Vehicle.java
@@ -0,0 +1,24 @@
+package org.example.autowiringbeanapproach;
+
+public class Vehicle {
+    public Vehicle(){
+        System.out.println("Vehicle bean created by Spring");
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
